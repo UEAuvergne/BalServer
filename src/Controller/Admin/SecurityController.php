cfbe2c153da628dcdf20787f816eefb9baa7a3e5
Avril Papillon <avril.papillon@proton.me>
2025-03-30 01:31:00 +0100

    feat: login requirement for /admin access

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Controller\Admin;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Response; 
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+use function PHPUnit\Framework\throwException;
 
 class SecurityController extends AbstractController
 {
@@ -17,7 +20,15 @@ class SecurityController extends AbstractController
         return $this->render('@EasyAdmin/page/login.html.twig', [
             'error' => $error,
             'last_username' => $lastUsername,
+
+            'csrf_token_intention' => "Token"
         ]);
+    }
+    
+    #[Route("/logout", name: "logout")]
+    public function logout()
+    {
+        return new Exception("Logout will be caught by the firewall");
     }
 }
 
