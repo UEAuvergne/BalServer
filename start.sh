cad96488cf958bbe8cad1a3a95f6b05606d20fc7
Avril Papillon <avril.papillon@proton.me>
2025-04-01 11:09:57 +0200

    make start.sh into a proper executable

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 docker run -v /var/www/balserver:/app -e SERVER_NAME=$SERVER_NAME -e FRANKENPHP_CONFIG="worker /app/public/index.php" -e APP_RUNTIME=Runtime\\FrankenPhpSymfony\\Runtime -p 80:80 -p 443:443 -p 443:443/udp dunglas/frankenphp
